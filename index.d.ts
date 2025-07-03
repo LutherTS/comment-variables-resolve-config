53ebde63122c6d39ad949b93808e7853b4078b18
Luther Tchofo Safo <nintendobserver@gmail.com>
2025-07-03 23:01:18 +0200

    No longer a Promise.

--- index.d.ts
+++ index.d.ts
@@ -5,7 +5,7 @@
  * @param {string} configPath The path of the config from `comments.config.js`, or from a config passed via the `--config` flag in the CLI, or from one passed via `"commentVariables.config": true` in `.vscode/settings.json` for the VS Code extension.
  * @returns The flattened config data, the reverse flattened config data, the verified config path, the raw passed ignores, and the original config. Errors are returned during failures so they can be reused differently on the CLI and the VS Code extension.
  */
-declare const resolveConfig: (configPath: string) => Promise<
+declare const resolveConfig: (configPath: string) =>
   | {
       success: false;
       errors: Array<{
@@ -24,8 +24,7 @@ declare const resolveConfig: (configPath: string) => Promise<
       reversedFlattenedConfigData: {
         [k: string]: string;
       };
-    }
->;
+    };
 
 export default resolveConfig;
 
