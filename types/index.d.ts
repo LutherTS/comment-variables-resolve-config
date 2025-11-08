f1c73551ed4d8fe5259de876707107f9558744f6
Luther Tchofo Safo <nintendobserver@gmail.com>
2025-11-08 18:23:45 +0100

    Compressed for dev.

--- types/index.d.ts
+++ types/index.d.ts
@@ -9,9 +9,9 @@ import type { Linter } from "eslint";
 // must be manually maintained
 
 /**
- * Verifies, validates and resolves the config path to retrieve the config's data, ignores, and more.
- * @param {string} configPath The path of the config from `comments.config.js`, or from a config passed via the `--config` flag in the CLI, or from one passed via `"commentVariables.config": "my.config.js"` in `.vscode/settings.json` for the VS Code extension.
- * @returns The flattened config data, the reverse flattened config data, the verified config path, the raw passed ignores, the original config, and more. Errors are returned during failures so they can be reused differently on the CLI and the VS Code extension.
+ * $COMMENT#JSDOC#DEFINITIONS#RESOLVECONFIG
+ * @param {string} configPath $COMMENT#JSDOC#PARAMS#CONFIGPATHA
+ * @returns $COMMENT#JSDOC#RETURNS#RESOLVECONFIG
  */
 declare const resolveConfig: (configPath: string) => Promise<
   | {
@@ -213,7 +213,7 @@ export const extractObjectStringLiteralValues: TSESLint.RuleModule<
   unknown
 >;
 
-/** The core data needed to run the "extract" rule, fully-named `"extract-object-string-literal-values"`. (The name of the object could eventually be changed for being too function-sounding, since it could be confused for "a function that extract rule config data" instead of what it is, "the data of the extract rule config".) */
+/** $COMMENT#JSDOC#CONSTANTS#EXTRACTRULECONFIGDATA */
 export const extractRuleConfigData: Readonly<{
   pluginName: "comment-variables";
   ruleName: "extract-object-string-literal-values";
@@ -259,19 +259,19 @@ export const extractRuleConfigData: Readonly<{
   >;
 }>;
 
-/** Ensures keys should only include lowercase letters (`Ll`), uppercase letters (`Lu`), other letters (`Lo`), dash punctuation (`Pd`), connector punctuation (`Pc`), numbers (`N`) and whitespaces (`s`). */
+/** $COMMENT#JSDOC#CONSTANTS#CONFIGKEYREGEX */
 export const configKeyRegex: RegExp;
-/** Same as `configKeyRegex` but without lowercase letters (`\p{Ll}`), without whitespaces (`\s` which are replaced by underscores) and with the '`#`' character (that links each subkey together). */
+/** $COMMENT#JSDOC#CONSTANTS#FLATTENEDCONFIGKEYREGEX */
 export const flattenedConfigKeyRegex: RegExp;
-/** Same as `flattenedConfigKeyRegex` but taking the prefix `$COMMENT` and its `#` into consideration, preventing two consecutive `#`'s, removing `^` and `$` in the capture group, and using `_` as replacement for whitespaces. */
+/** $COMMENT#JSDOC#CONSTANTS#FLATTENEDCONFIGPLACEHOLDERLOCALREGEX */
 export const flattenedConfigPlaceholderLocalRegex: RegExp;
-/** Same as `flattenedConfigPlaceholderLocalRegex` but globally. */
+/** $COMMENT#JSDOC#CONSTANTS#FLATTENEDCONFIGPLACEHOLDERGLOBALREGEX */
 export const flattenedConfigPlaceholderGlobalRegex: RegExp;
 
 /**
- * Makes a `{success: false}` object with a single error in its errors array of `{type: "error"}` based on the message it is meant to display.
- * @param {string} message The human-readable message of the error.
- * @returns A `{success: false}` object with a single error in its error array of `{type: "error"}`.
+ * $COMMENT#JSDOC#DEFINITIONS#MAKESUCCESSFALSETYPEERROR
+ * @param {string} message $COMMENT#JSDOC#PARAMS#MESSAGE
+ * @returns $COMMENT#JSDOC#RETURNS#MAKESUCCESSFALSETYPEERROR
  */
 export const makeSuccessFalseTypeError: (message: string) => {
   errors: {
@@ -282,11 +282,11 @@ export const makeSuccessFalseTypeError: (message: string) => {
 };
 
 /**
- * Extracts and format the output JSON from an ESLint rule's `context.report` to turn it into Value Locations.
- * @param {LintMessage[]} lintMessages The array of LintMessages such as obtained from an `ESLint` or a `Linter` instance running.
- * @param {string} pluginName The name of the plugin being used for filtering.
- * @param {string} ruleName The name of the rule being used for filtering.
- * @returns An array of Value Locations with the value, the file path and the SourceLocation (LOC) included for each.
+ * $COMMENT#JSDOC#DEFINITIONS#EXTRACTVALUELOCATIONSFROMLINTMESSAGES
+ * @param {LintMessage[]} lintMessages $COMMENT#JSDOC#PARAMS#LINTMESSAGES
+ * @param {string} pluginName $COMMENT#JSDOC#PARAMS#PLUGINNAME
+ * @param {string} ruleName $COMMENT#JSDOC#PARAMS#RULENAME
+ * @returns $COMMENT#JSDOC#RETURNS#EXTRACTVALUELOCATIONSFROMLINTMESSAGES
  */
 export const extractValueLocationsFromLintMessages: (
   lintMessages: Linter.LintMessage[],
@@ -299,34 +299,34 @@ export const extractValueLocationsFromLintMessages: (
 }[];
 
 /**
- * Escapes all regex characters with a `"\"` in a string to prepare it for use in a regex.
- * @param {string} string The string.
- * @returns The string with regex characters escaped.
+ * $COMMENT#JSDOC#DEFINITIONS#ESCAPEREGEX
+ * @param {string} string $COMMENT#JSDOC#PARAMS#STRINGA
+ * @returns $COMMENT#JSDOC#RETURNS#ESCAPEREGEX
  */
 export const escapeRegex: (string: string) => string;
 
 /**
- * Makes a global regex for a given string that ensures it is surrounded by whitespace.
- * @param {string} string The string.
- * @returns The regex complete with positive lookbehind and positive lookahead to ensure the string is taken into account only when surrounded by whitespace.
+ * $COMMENT#JSDOC#DEFINITIONS#MAKEISOLATEDSTRINGREGEX
+ * @param {string} string $COMMENT#JSDOC#PARAMS#STRINGA
+ * @returns $COMMENT#JSDOC#RETURNS#MAKEISOLATEDSTRINGREGEX
  */
 export const makeIsolatedStringRegex: (string: string) => RegExp;
 
 /**
- * Removes the variant prefix of a Comment Variable placeholder.
- * @param {string} variationPlaceholder The variation placeholder that needs its variant prefix removed.
- * @returns The variation placeholder with its variant prefix removed, akin to a Comment Variable placeholder when `variations` are not in use.
+ * $COMMENT#JSDOC#DEFINITIONS#REMOVEVARIANTPREFIXFROMVARIATIONPLACEHOLDER
+ * @param {string} variationPlaceholder $COMMENT#JSDOC#PARAMS#VARIATIONPLACEHOLDER
+ * @returns $COMMENT#JSDOC#RETURNS#REMOVEVARIANTPREFIXFROMVARIATIONPLACEHOLDER
  */
 export const removeVariantPrefixFromVariationPlaceholder: (
   variationPlaceholder: string
 ) => string;
 
 /**
- * Creates that object with the same keys and the same base shape as the original config data now with all string values entirely resolved alongside Comment Variables keys.
- * @param {Record<string, unknown>} configData The original config data obtained from resolveConfig.
- * @param {Record<string, string>} flattenedConfigData The flattened config data obtained from resolveConfig.
- * @param {Record<string, string>} aliases_flattenedKeys The aliases-to-flattened-keys dictionary obtained from resolveConfig.
- * @returns An object with `success: true` and the resolved config data if successful, or with `success: false` and errors if unsuccessful.
+ * $COMMENT#JSDOC#DEFINITIONS#MAKERESOLVEDCONFIGDATA
+ * @param {Record<string, unknown>} configData $COMMENT#JSDOC#PARAMS#CONFIGDATAB
+ * @param {Record<string, string>} flattenedConfigData $COMMENT#JSDOC#PARAMS#FLATTENEDCONFIGDATAB
+ * @param {Record<string, string>} aliases_flattenedKeys $COMMENT#JSDOC#PARAMS#ALIASES_FLATTENEDKEYS
+ * @returns $COMMENT#JSDOC#RETURNS#MAKERESOLVEDCONFIGDATA
  */
 export const makeResolvedConfigData: (
   configData: Record<string, unknown>,
@@ -346,33 +346,33 @@ export const makeResolvedConfigData: (
     };
 
 /**
- * Makes the JSON resolved config data to be written at an expected `.json` path.
- * @param {Record<string, unknown>} resolvedConfigData The resolved config data as obtained from `makeResolvedConfigData`.
- * @returns The JSON resolved config data to be written at an expected `.json` path. It can be consumed by any language that can parse JSON, which means virtually all modern languages, so that Comment Variables can act as a single source of truth for text variables beyond JavaScript and TypeScript.
+ * $COMMENT#JSDOC#DEFINITIONS#MAKEJSONDATA
+ * @param {Record<string, unknown>} resolvedConfigData $COMMENT#JSDOC#PARAMS#RESOLVEDCONFIGDATAB
+ * @returns $COMMENT#JSDOC#RETURNS#MAKEJSONDATA
  */
 export const makeJsonData: (
   resolvedConfigData: Record<string, unknown>
 ) => string;
 
 /**
- * Makes the MJS resolved config data to be written at an expected `.mjs` path.
- * @param {Record<string, unknown>} resolvedConfigData The resolved config data as obtained from `makeResolvedConfigData`.
- * @returns The MJS resolved config data to be written at an expected `.mjs` path. Its format makes it possible to be consumed with literal type safety in both JavaScript and TypeScript.
+ * $COMMENT#JSDOC#DEFINITIONS#MAKEMJSDATA
+ * @param {Record<string, unknown>} resolvedConfigData $COMMENT#JSDOC#PARAMS#RESOLVEDCONFIGDATAB
+ * @returns $COMMENT#JSDOC#RETURNS#MAKEMJSDATA
  */
 export const makeMjsData: (
   resolvedConfigData: Record<string, unknown>
 ) => string;
 
 /**
- * Makes the log that announces the writing of the JSON resolved config data.
- * @param {string} jsonPath The expected `.json` path where the JSON resolved config data is to be written.
- * @returns The log that announces the writing of the JSON resolved config data has been completed.
+ * $COMMENT#JSDOC#DEFINITIONS#MAKEJSONPATHLOG
+ * @param {string} jsonPath $COMMENT#JSDOC#PARAMS#JSONPATH
+ * @returns $COMMENT#JSDOC#RETURNS#MAKEJSONPATHLOG
  */
 export const makeJsonPathLog: (jsonPath: string) => string;
 
 /**
- * Makes the log that announces the writing of the MJS resolved config data.
- * @param {string} mjsPath The expected `.mjs` path where the MJS resolved config data is to be written.
- * @returns The log that announces the writing of the MJS resolved config data has been completed.
+ * $COMMENT#JSDOC#DEFINITIONS#MAKEMJSPATHLOG
+ * @param {string} mjsPath $COMMENT#JSDOC#PARAMS#MJSPATH
+ * @returns $COMMENT#JSDOC#RETURNS#MAKEMJSPATHLOG
  */
 export const makeMjsPathLog: (mjsPath: string) => string;
