1b70ea38067d0438e54d5c4dfbac2213edfd0a28
Luther Tchofo Safo <nintendobserver@gmail.com>
2025-11-08 18:08:33 +0100

    Compressed for dev.

--- library/_commons/constants/regexes.js
+++ library/_commons/constants/regexes.js
@@ -2,20 +2,20 @@ import { $COMMENT } from "./bases.js";
 
 import { escapeRegex } from "../utilities/helpers.js";
 
-/** Ensures keys should only include lowercase letters (`Ll`), uppercase letters (`Lu`), other letters (`Lo`), dash punctuation (`Pd`), connector punctuation (`Pc`), numbers (`N`) and whitespaces (`s`). */
+/** $COMMENT#JSDOC#CONSTANTS#CONFIGKEYREGEX */
 export const configKeyRegex = /^[\p{Ll}\p{Lu}\p{Lo}\p{Pd}\p{Pc}\p{N}\s]+$/u;
 
-/** Same as `configKeyRegex` but without lowercase letters (`\p{Ll}`), without whitespaces (`\s` which are replaced by underscores) and with the '`#`' character (that links each subkey together). */
+/** $COMMENT#JSDOC#CONSTANTS#FLATTENEDCONFIGKEYREGEX */
 export const flattenedConfigKeyRegex =
   /^(?!#)[\p{Lu}\p{Lo}\p{Pd}\p{Pc}\p{N}#]+$/u;
 
-/** Same as `flattenedConfigKeyRegex` but taking the prefix `$COMMENT` and its `#` into consideration, preventing two consecutive `#`'s, removing `^` and `$` in the capture group, and using `_` as replacement for whitespaces. */
+/** $COMMENT#JSDOC#CONSTANTS#FLATTENEDCONFIGPLACEHOLDERLOCALREGEX */
 export const flattenedConfigPlaceholderLocalRegex = new RegExp(
   `${escapeRegex($COMMENT)}#(?!#)([\\p{Lu}\\p{Lo}\\p{Pd}\\p{Pc}\\p{N}#_]+)`,
   "u"
 );
 
-/** Same as `flattenedConfigPlaceholderLocalRegex` but globally. */
+/** $COMMENT#JSDOC#CONSTANTS#FLATTENEDCONFIGPLACEHOLDERGLOBALREGEX */
 export const flattenedConfigPlaceholderGlobalRegex = new RegExp(
   `${escapeRegex($COMMENT)}#(?!#)([\\p{Lu}\\p{Lo}\\p{Pd}\\p{Pc}\\p{N}#_]+)`,
   "gu"
