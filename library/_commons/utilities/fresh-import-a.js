7d3e2783f4d99a452f6376f7931aa1e9f52611d8
Luther Tchofo Safo <nintendobserver@gmail.com>
2026-05-02 01:34:25 +0200

    Resolved for prod.

--- library/_commons/utilities/fresh-import-a.js
+++ library/_commons/utilities/fresh-import-a.js
@@ -10,9 +10,9 @@ const childScriptAbsolutePath = path.join(
 );
 
 /**
- * $COMMENT#JSDOC#DEFINITIONS#FRESHIMPORT
- * @param {string} moduleUrl $COMMENT#JSDOC#PARAMS#MODULEURL
- * @returns $COMMENT#JSDOC#RETURNS#FRESHIMPORT
+ * Guarantees a fresh import of the config, negating the innate (and hidden) cache of the dynamic `import` utility.
+ * @param {string} moduleUrl The absolute path of the module to import.
+ * @returns Either an object with its `default` property sets to the default export of the module successfully loaded or `null` when an error arises. (Debugging is currently manual by looking at the error being caught in the child process.)
  */
 export async function freshImport(moduleUrl) {
   const childProcess = fork(childScriptAbsolutePath, {
